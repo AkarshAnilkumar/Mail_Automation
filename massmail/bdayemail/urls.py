8005520026cb9e4fa84c64bbb9ebf878e5e5d87d
vidhya-vjkumar <106170027+vidhya-vjkumar@users.noreply.github.com>
2023-02-25 09:05:27 +0530

    Delete massmail directory

--- massmail/bdayemail/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-#from django.conf.urls import patterns,url
-
-urlpatterns = [
-    path('',views.sendBdayEmail,name='sendBdayEmail')
-    #path('',views.sendSimpleMail,name='sendSimpleMail')
-    #patterns('myapp.views',url(r'^simpleemail/(?P<emailto>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/''sendSimpleMail',ame='sendSimpleMail'),)
-]
